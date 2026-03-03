ed9b544c9452e8f059eb17099cfa34be08192436
Danilo Correia <danilocorreiaviana@hotmail.com>
2026-03-03 14:59:39 -0300

    Initialize Express server with MongoDB connection

---
+++ api/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const alunoRoute = require('../route/alunoRoute');
+const grupoRoute = require('../route/grupoRoute');
+
+const app = express();
+
+// Conexão Mongo (forma correta para serverless)
+if (!mongoose.connections[0].readyState) {
+    mongoose.connect(process.env.DB_CONCT)
+        .then(() => console.log('Mongo conectado'))
+        .catch(err => console.log('Erro Mongo:', err));
+}
+
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// Rotas
+app.use(alunoRoute);
+app.use(grupoRoute);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
+module.exports = app;
