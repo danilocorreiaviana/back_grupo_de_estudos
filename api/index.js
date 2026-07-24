5bceadd9215f74a500e97b035afe9ec8cda6c507
Danilo Correia <danilocorreiaviana@hotmail.com>
2026-07-24 15:22:03 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const alunoRoute = require('./route/alunoRoute');
-const grupoRoute = require('./route/grupoRoute');
+const alunoRoute = require('../route/alunoRoute');
+const grupoRoute = require('../route/grupoRoute');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
