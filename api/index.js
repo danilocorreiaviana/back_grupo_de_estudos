a9782164281d5c4a7721de44bfd342ddcd612855
Danilo Correia <danilocorreiaviana@hotmail.com>
2026-07-24 13:04:28 -0300

    Refactor MongoDB connection handling

--- api/index.js
+++ api/index.js
@@ -9,26 +9,56 @@ const grupoRoute = require('../route/grupoRoute');
 
 const app = express();
 
-// Conexão Mongo (forma correta para serverless)
-if (!mongoose.connections[0].readyState) {
-    mongoose.connect(process.env.DB_CONCT)
-        .then(() => console.log('Mongo conectado'))
-        .catch(err => console.log('Erro Mongo:', err));
+
+let cachedConnection = global.mongooseConnection;
+
+if (!cachedConnection) {
+    cachedConnection = global.mongooseConnection = {
+        conn: null,
+        promise: null
+    };
 }
+
+async function connectMongo() {
+    if (cachedConnection.conn) {
+        return cachedConnection.conn;
+    }
+
+    if (!cachedConnection.promise) {
+        cachedConnection.promise = mongoose.connect(process.env.DB_CONCT)
+            .then((mongooseInstance) => {
+                console.log('Mongo conectado');
+                return mongooseInstance;
+            })
+            .catch((err) => {
+                console.log('Erro Mongo:', err);
+                throw err;
+            });
+    }
+
+    cachedConnection.conn = await cachedConnection.promise;
+
+    return cachedConnection.conn;
+}
+
+connectMongo();
 
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 app.use(express.json());
 app.use(cors());
 
+
 // Rotas
 app.use(alunoRoute);
 app.use(grupoRoute);
+
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: 'public' });
 });
+
 
 module.exports = app;
